de834ce81f492428e5dd81ab3b3f184ede173486
Vadim <vadimgordenya@gmail.com>
2017-01-08 15:36:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+$(window).load(function() {
+        $(".loader__wrapp").css("display", "none");
+});
 $(document).ready(function(){
-        	$(".loader__wrapp").css("display", "none");
-    	
-
 	$(".tab__first").click(function(){
 		$(".first").css("display","inline-block");
 		$(".second").css("display","none");
